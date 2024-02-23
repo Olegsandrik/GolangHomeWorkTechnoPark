33adf97913139a92a6fa97adc30c29f27baf334a
Olegsandrik <Nikitinnvlalex@gmail.com>
2024-02-23 21:10:33 +0300

    Добавил все логические флаги, остались ввод/вывод + тесты

--- uniq.go
+++ uniq.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 )
 
-// запуск echo -e "небо\nоблака\nоблака\nоблака\nрозы\nрозы\n" | uniq [-c | -d | -u] [-i] [-f num] [-s chars] [input_file [output_file]]
 /*
 -с - подсчитать количество встречаний строки во входных данных. Вывести это число перед строкой отделив пробелом.
 
@@ -44,12 +43,9 @@ func Uniq(stringsIn []string, option Options) []string {
 	)
 
 	numChars := option.S
+	numFields := option.F
 
-	// Не учитывать регистр букв
-	// strings.ToLower()
-
-	// Учитывать регистр букв.
-	// Соблюдаем CDU или его отсутствие
+	// Соблюдаем CDU или его отсутствие и все остальные флаги
 
 	counterUniq := 0
 	currentString := stringsIn[0]
@@ -57,9 +53,36 @@ func Uniq(stringsIn []string, option Options) []string {
 	stringsOutWithout = append(stringsOut, currentString)
 	stringsOutWithC = append(stringsOut, currentString)
 	for i := 1; i < len(stringsIn); i++ {
+		currentPointer := 0
+		stringPointer := 0
+		// Выполним условие флага f
+		if numFields > 0 {
+			countFieldCurrent := 0
+			for j := 0; j < len(currentString)-1; j++ {
+				if currentString[j] != ' ' && currentString[j+1] == ' ' {
+					countFieldCurrent++
+					if countFieldCurrent == numFields {
+						currentPointer = j + 1
+						break
+					}
+				}
+			}
+
+			countFieldString := 0
+			for k := 0; k < len(stringsIn[i])-1; k++ {
+				if stringsIn[i][k] != ' ' && stringsIn[i][k+1] == ' ' {
+					countFieldString++
+					if countFieldString == numFields {
+						stringPointer = k + 1
+						break
+					}
+				}
+			}
+		}
+
 		if option.I {
 			// Не учитывать регистр
-			if strings.ToLower(currentString[numChars:]) == strings.ToLower(stringsIn[i][numChars:]) {
+			if strings.ToLower(currentString[(numChars+currentPointer):]) == strings.ToLower(stringsIn[i][(numChars+stringPointer):]) {
 				counterUniq++
 				continue
 			} else {
@@ -71,7 +94,7 @@ func Uniq(stringsIn []string, option Options) []string {
 			}
 		} else {
 			// Учитывать регистр
-			if currentString[numChars:] == stringsIn[i][numChars:] {
+			if currentString[(numChars+currentPointer):] == stringsIn[i][(numChars+stringPointer):] {
 				counterUniq++
 				continue
 			} else {
@@ -84,8 +107,8 @@ func Uniq(stringsIn []string, option Options) []string {
 		}
 
 	}
-	stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
 
+	stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
 	for i := 1; i < len(stringsOutWithC); {
 		currentCount, _ := strconv.Atoi(stringsOutWithC[i])
 		if currentCount > 1 {
@@ -96,7 +119,7 @@ func Uniq(stringsIn []string, option Options) []string {
 		i += 2
 	}
 
-	// теперь у нас есть 4 массива для каждого из вариантов CDU или его отсутствие
+	// теперь у нас есть 4 массива для каждого из вариантов CDU или его отсутствие и все остальные флаги
 
 	if option.C {
 		// Подсчитать количество встречаний строки во входных данных. Вывести это число перед строкой отделив пробелом.
@@ -111,145 +134,6 @@ func Uniq(stringsIn []string, option Options) []string {
 		stringsOut = stringsOutWithD
 	} else if option.U {
 		// Вывести только те строки, которые не повторились во входных данных.
-		stringsOut = stringsOutWithU
-	} else {
-		stringsOut = stringsOutWithout
-	}
-
-	if option.F > 0 {
-		// Не учитывать первые num_fields полей в строке. Полем в строке является непустой набор символов отделённый пробелом.
-	}
-
-	if option.S > 0 {
-		// Не учитывать первые num_chars символов в строке. При использовании вместе с параметром
-		// -f учитываются первые символы после num_fields полей (не учитывая пробел-разделитель после последнего поля).
-	}
-
-	if len(option.Input) > 0 {
-		if len(option.Output) > 0 {
-			// тут оба указаны
-		}
-		// тут о только ввод
-	}
-
-	return stringsOut
-}
-
-// Uniq - Функция уникализации
-func BadUniq(stringsIn []string, option Options) []string {
-	var (
-		stringsOut        []string
-		stringsOutWithC   []string
-		stringsOutWithD   []string
-		stringsOutWithU   []string
-		stringsOutWithout []string
-	)
-
-	numChars := option.S
-	/*
-		numFields := option.F
-		currentPointer := 0
-		stringPointer := 0
-		//*/
-
-	// Соблюдаем CDU или его отсутствие
-
-	counterUniq := 0
-	currentString := stringsIn[0]
-	counterUniq++
-	stringsOutWithout = append(stringsOut, currentString)
-	stringsOutWithC = append(stringsOut, currentString)
-	for i := 1; i < len(stringsIn); i++ {
-		// Вычислим ограничение по numFields для обеих строчек
-		/*
-			countField1 := 0
-			for j := 0; j < len(currentString)-1; j++ {
-
-				if numFields == 0 {
-					break
-				}
-				if currentString[j] != ' ' {
-					if currentString[j+1] == ' ' {
-						countField1++
-						if countField1 == numFields {
-							currentPointer = j
-							break
-						}
-					}
-				}
-			}
-
-			countField2 := 0
-			for j := 0; j < len(stringsIn[i])-1; j++ {
-
-				if numFields == 0 {
-					break
-				}
-				if stringsIn[i][j] != ' ' {
-					if stringsIn[i][j+1] == ' ' {
-						countField2++
-						if countField2 == numFields {
-							stringPointer = j
-							break
-						}
-					}
-				}
-			}
-			currentString = currentString[currentPointer:]
-			stringsIn = stringsIn[stringPointer:]
-
-			//*/
-		if option.I {
-			// Не учитывать регистр букв
-			if strings.ToLower(currentString[numChars:]) == strings.ToLower(stringsIn[i][numChars:]) {
-				counterUniq++
-				continue
-			} else {
-				currentString = stringsIn[i]
-				stringsOutWithout = append(stringsOutWithout, currentString)
-				stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
-				stringsOutWithC = append(stringsOutWithC, currentString)
-				counterUniq = 1
-			}
-		} else {
-			// Учитывать регистр букв.
-
-			if currentString[0:] == stringsIn[i][numChars:] {
-				counterUniq++
-				continue
-			} else {
-				currentString = stringsIn[i]
-				stringsOutWithout = append(stringsOutWithout, currentString)
-				stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
-				stringsOutWithC = append(stringsOutWithC, currentString)
-				counterUniq = 1
-			}
-		}
-
-	}
-
-	stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
-	for i := 1; i < len(stringsOutWithC); {
-		currentCount, _ := strconv.Atoi(stringsOutWithC[i])
-		if currentCount > 1 {
-			stringsOutWithD = append(stringsOutWithD, stringsOutWithC[i-1])
-		} else {
-			stringsOutWithU = append(stringsOutWithU, stringsOutWithC[i-1])
-		}
-		i += 2
-	}
-
-	// теперь у нас есть 4 массива для каждого из вариантов CDU или его отсутствие, с учетом флагов i, s, f
-	if option.C {
-		result := []string{}
-		for i := 1; i < len(stringsOutWithC); {
-			result = append(result, stringsOutWithC[i]+" "+stringsOutWithC[i-1])
-			i += 2
-		}
-		stringsOut = result
-	} else if option.D {
-		stringsOut = stringsOutWithD
-	} else if option.U {
 		stringsOut = stringsOutWithU
 	} else {
 		stringsOut = stringsOutWithout
@@ -271,7 +155,6 @@ func main() {
 		"Thanks.", "I love music of kartik.", "I love MuSIC of Kartik."} // С флагом I
 	stringsIn6 := []string{"We love music.", "I love music.", "They love music.", " ", "I love music of Kartik.", "We love music of Kartik.",
 		"Thanks"} // С флагом F
-
 	stringsIn7 := []string{"I love music.", "A love music.", "C love music.", " ", "I love music of Kartik.", "We love music of Kartik.",
 		"Thanks."} // С флагом S
 	// вывод и ввод нашей утилиты
