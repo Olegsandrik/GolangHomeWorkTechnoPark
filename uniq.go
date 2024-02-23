c65948c252fe53f06768013666a646ec44c5c027
Olegsandrik <Nikitinnvlalex@gmail.com>
2024-02-23 20:01:13 +0300

    Добавил все поинты, кроме input/output и -f

--- uniq.go
+++ uniq.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"strconv"
+	"strings"
 )
 
 // запуск echo -e "небо\nоблака\nоблака\nоблака\nрозы\nрозы\n" | uniq [-c | -d | -u] [-i] [-f num] [-s chars] [input_file [output_file]]
@@ -42,20 +43,35 @@ func Uniq(stringsIn []string, option Options) []string {
 		stringsOutWithout []string
 	)
 
-	if option.I {
-		// Не учитывать регистр букв
-		// strings.ToLower()
-	} else {
-		// Учитывать регистр букв.
-		// Соблюдаем CDU или его отсутствие
+	numChars := option.S
 
-		counterUniq := 0
-		currentString := stringsIn[0]
-		counterUniq++
-		stringsOutWithout = append(stringsOut, currentString)
-		stringsOutWithC = append(stringsOut, currentString)
-		for i := 1; i < len(stringsIn); i++ {
-			if currentString == stringsIn[i] {
+	// Не учитывать регистр букв
+	// strings.ToLower()
+
+	// Учитывать регистр букв.
+	// Соблюдаем CDU или его отсутствие
+
+	counterUniq := 0
+	currentString := stringsIn[0]
+	counterUniq++
+	stringsOutWithout = append(stringsOut, currentString)
+	stringsOutWithC = append(stringsOut, currentString)
+	for i := 1; i < len(stringsIn); i++ {
+		if option.I {
+			// Не учитывать регистр
+			if strings.ToLower(currentString[numChars:]) == strings.ToLower(stringsIn[i][numChars:]) {
+				counterUniq++
+				continue
+			} else {
+				currentString = stringsIn[i]
+				stringsOutWithout = append(stringsOutWithout, currentString)
+				stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
+				stringsOutWithC = append(stringsOutWithC, currentString)
+				counterUniq = 1
+			}
+		} else {
+			// Учитывать регистр
+			if currentString[numChars:] == stringsIn[i][numChars:] {
 				counterUniq++
 				continue
 			} else {
@@ -66,37 +82,38 @@ func Uniq(stringsIn []string, option Options) []string {
 				counterUniq = 1
 			}
 		}
-		stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
 
+	}
+	stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
+
+	for i := 1; i < len(stringsOutWithC); {
+		currentCount, _ := strconv.Atoi(stringsOutWithC[i])
+		if currentCount > 1 {
+			stringsOutWithD = append(stringsOutWithD, stringsOutWithC[i-1])
+		} else {
+			stringsOutWithU = append(stringsOutWithU, stringsOutWithC[i-1])
+		}
+		i += 2
+	}
+
+	// теперь у нас есть 4 массива для каждого из вариантов CDU или его отсутствие
+
+	if option.C {
+		// Подсчитать количество встречаний строки во входных данных. Вывести это число перед строкой отделив пробелом.
+		result := []string{}
 		for i := 1; i < len(stringsOutWithC); {
-			currentCount, _ := strconv.Atoi(stringsOutWithC[i])
-			if currentCount > 1 {
-				stringsOutWithD = append(stringsOutWithD, stringsOutWithC[i-1])
-			} else {
-				stringsOutWithU = append(stringsOutWithU, stringsOutWithC[i-1])
-			}
+			result = append(result, stringsOutWithC[i]+" "+stringsOutWithC[i-1])
 			i += 2
 		}
-
-		// теперь у нас есть 4 массива для каждого из вариантов CDU или его отсутствие
-
-		if option.C {
-			// Подсчитать количество встречаний строки во входных данных. Вывести это число перед строкой отделив пробелом.
-			result := []string{}
-			for i := 1; i < len(stringsOutWithC); {
-				result = append(result, stringsOutWithC[i]+" "+stringsOutWithC[i-1])
-				i += 2
-			}
-			stringsOut = result
-		} else if option.D {
-			// Вывести только те строки, которые повторились во входных данных.
-			stringsOut = stringsOutWithD
-		} else if option.U {
-			// Вывести только те строки, которые не повторились во входных данных.
-			stringsOut = stringsOutWithU
-		} else {
-			stringsOut = stringsOutWithout
-		}
+		stringsOut = result
+	} else if option.D {
+		// Вывести только те строки, которые повторились во входных данных.
+		stringsOut = stringsOutWithD
+	} else if option.U {
+		// Вывести только те строки, которые не повторились во входных данных.
+		stringsOut = stringsOutWithU
+	} else {
+		stringsOut = stringsOutWithout
 	}
 
 	if option.F > 0 {
@@ -117,12 +134,207 @@ func Uniq(stringsIn []string, option Options) []string {
 
 	return stringsOut
 }
-func main() {
-	stringsIn := []string{"небо", "НЕБО", "облака", "облака", "облака", "розы", "розы"}
-	option := Options{} // заполняем в соответствии с параметрами
-	stringsOut := Uniq(stringsIn, option)
 
-	for i := 0; i < len(stringsOut); i++ {
-		log.Println(stringsOut[i])
+// Uniq - Функция уникализации
+func BadUniq(stringsIn []string, option Options) []string {
+	var (
+		stringsOut        []string
+		stringsOutWithC   []string
+		stringsOutWithD   []string
+		stringsOutWithU   []string
+		stringsOutWithout []string
+	)
+
+	numChars := option.S
+	/*
+		numFields := option.F
+		currentPointer := 0
+		stringPointer := 0
+		//*/
+
+	// Соблюдаем CDU или его отсутствие
+
+	counterUniq := 0
+	currentString := stringsIn[0]
+	counterUniq++
+	stringsOutWithout = append(stringsOut, currentString)
+	stringsOutWithC = append(stringsOut, currentString)
+	for i := 1; i < len(stringsIn); i++ {
+		// Вычислим ограничение по numFields для обеих строчек
+		/*
+			countField1 := 0
+			for j := 0; j < len(currentString)-1; j++ {
+
+				if numFields == 0 {
+					break
+				}
+				if currentString[j] != ' ' {
+					if currentString[j+1] == ' ' {
+						countField1++
+						if countField1 == numFields {
+							currentPointer = j
+							break
+						}
+					}
+				}
+			}
+
+			countField2 := 0
+			for j := 0; j < len(stringsIn[i])-1; j++ {
+
+				if numFields == 0 {
+					break
+				}
+				if stringsIn[i][j] != ' ' {
+					if stringsIn[i][j+1] == ' ' {
+						countField2++
+						if countField2 == numFields {
+							stringPointer = j
+							break
+						}
+					}
+				}
+			}
+			currentString = currentString[currentPointer:]
+			stringsIn = stringsIn[stringPointer:]
+
+			//*/
+		if option.I {
+			// Не учитывать регистр букв
+			if strings.ToLower(currentString[numChars:]) == strings.ToLower(stringsIn[i][numChars:]) {
+				counterUniq++
+				continue
+			} else {
+				currentString = stringsIn[i]
+				stringsOutWithout = append(stringsOutWithout, currentString)
+				stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
+				stringsOutWithC = append(stringsOutWithC, currentString)
+				counterUniq = 1
+			}
+		} else {
+			// Учитывать регистр букв.
+
+			if currentString[0:] == stringsIn[i][numChars:] {
+				counterUniq++
+				continue
+			} else {
+				currentString = stringsIn[i]
+				stringsOutWithout = append(stringsOutWithout, currentString)
+				stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
+				stringsOutWithC = append(stringsOutWithC, currentString)
+				counterUniq = 1
+			}
+		}
+
 	}
+
+	stringsOutWithC = append(stringsOutWithC, strconv.Itoa(counterUniq))
+	for i := 1; i < len(stringsOutWithC); {
+		currentCount, _ := strconv.Atoi(stringsOutWithC[i])
+		if currentCount > 1 {
+			stringsOutWithD = append(stringsOutWithD, stringsOutWithC[i-1])
+		} else {
+			stringsOutWithU = append(stringsOutWithU, stringsOutWithC[i-1])
+		}
+		i += 2
+	}
+
+	// теперь у нас есть 4 массива для каждого из вариантов CDU или его отсутствие, с учетом флагов i, s, f
+	if option.C {
+		result := []string{}
+		for i := 1; i < len(stringsOutWithC); {
+			result = append(result, stringsOutWithC[i]+" "+stringsOutWithC[i-1])
+			i += 2
+		}
+		stringsOut = result
+	} else if option.D {
+		stringsOut = stringsOutWithD
+	} else if option.U {
+		stringsOut = stringsOutWithU
+	} else {
+		stringsOut = stringsOutWithout
+	}
+
+	return stringsOut
+}
+
+func main() {
+	stringsIn1 := []string{"I love music.", "I love music.", "I love music.", " ", "I love music of Kartik.", "I love music of Kartik.",
+		"Thanks.", "I love music of Kartik.", "I love music of Kartik."} // Обычный
+	stringsIn2 := []string{"I love music.", "I love music.", "I love music.", " ", "I love music of Kartik.", "I love music of Kartik.",
+		"Thanks.", "I love music of Kartik.", "I love music of Kartik."} // С флагом C
+	stringsIn3 := []string{"I love music.", "I love music.", "I love music.", " ", "I love music of Kartik.", "I love music of Kartik.",
+		"Thanks.", "I love music of Kartik.", "I love music of Kartik."} // С флагом D
+	stringsIn4 := []string{"I love music.", "I love music.", "I love music.", " ", "I love music of Kartik.", "I love music of Kartik.",
+		"Thanks.", "I love music of Kartik.", "I love music of Kartik."} // С флагом U
+	stringsIn5 := []string{"I LOVE MUSIC.", "I love music.", "I LoVe MuSiC.", " ", "I love MuSIC of Kartik.", "I love music of kartik.",
+		"Thanks.", "I love music of kartik.", "I love MuSIC of Kartik."} // С флагом I
+	stringsIn6 := []string{"We love music.", "I love music.", "They love music.", " ", "I love music of Kartik.", "We love music of Kartik.",
+		"Thanks"} // С флагом F
+
+	stringsIn7 := []string{"I love music.", "A love music.", "C love music.", " ", "I love music of Kartik.", "We love music of Kartik.",
+		"Thanks."} // С флагом S
+	// вывод и ввод нашей утилиты
+	/*
+		if len(option.Input) > 0 {
+			if len(option.Output) > 0 {
+				// тут оба указаны
+			}
+			// тут о только ввод
+		}
+	*/
+	option1 := Options{}
+	stringsOut1 := Uniq(stringsIn1, option1)
+	for i := 0; i < len(stringsOut1); i++ {
+		log.Println(stringsOut1[i])
+	}
+
+	log.Println("Пошел 2 тест")
+
+	option2 := Options{C: true}
+	stringsOut2 := Uniq(stringsIn2, option2)
+	for i := 0; i < len(stringsOut2); i++ {
+		log.Println(stringsOut2[i])
+	}
+
+	log.Println("Пошел 3 тест")
+
+	option3 := Options{D: true}
+	stringsOut3 := Uniq(stringsIn3, option3)
+	for i := 0; i < len(stringsOut3); i++ {
+		log.Println(stringsOut3[i])
+	}
+
+	log.Println("Пошел 4 тест")
+
+	option4 := Options{U: true}
+	stringsOut4 := Uniq(stringsIn4, option4)
+	for i := 0; i < len(stringsOut4); i++ {
+		log.Println(stringsOut4[i])
+	}
+
+	log.Println("Пошел 5 тест")
+
+	option5 := Options{I: true}
+	stringsOut5 := Uniq(stringsIn5, option5)
+	for i := 0; i < len(stringsOut5); i++ {
+		log.Println(stringsOut5[i])
+	}
+
+	log.Println("Пошел 6 тест")
+
+	option6 := Options{F: 1}
+	stringsOut6 := Uniq(stringsIn6, option6)
+	for i := 0; i < len(stringsOut6); i++ {
+		log.Println(stringsOut6[i])
+	}
+
+	log.Println("Пошел 7 тест")
+
+	option7 := Options{S: 1}
+	stringsOut7 := Uniq(stringsIn7, option7)
+	for i := 0; i < len(stringsOut7); i++ {
+		log.Println(stringsOut7[i])
+	}
+
 }
