4a36f54f6f93a553a5ba81e0834946dfaf67cb49
Olegsandrik <Nikitinnvlalex@gmail.com>
2024-02-26 20:08:24 +0300

    Переделал тесты

--- uniq.go
+++ uniq.go
@@ -6,8 +6,9 @@ import (
 )
 
 func main() {
-	// go run uniq.go -c -i -f 1 -s 1 input.txt output.txt
-	// cat input.txt | go run uniq.go -c -i -f 1 -s 1 input.txt output.txt
+	// Запуск:
+	// go run uniq.go -c -i -f=0 -s=0 -input=input.txt -output=output.txt
+	// cat input.txt | go run uniq.go -c -i -f=1 -s=1 -output=output.txt
 	option, err := inOut.Scanner()
 	if err != nil {
 		fmt.Println(err)
